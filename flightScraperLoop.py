0078db603d1f57da354bc8d5cf4e2cdad460ea3e
mackeynichols <mackey.nichols@gmail.com>
2017-08-24 07:25:20 -0400

    reduced code, added more command line arguments, only search for first flight

--- flightScraperLoop.py
+++ flightScraperLoop.py
@@ -2,11 +2,11 @@
 # flightScraper.py - loops through tailored json inputs for google flights api
 
 # COMMAND LINE INPUTS
-# 1: airport
-# 2: minimum price
-# 3: gmail password
-# 4: optional departure day (thursday or friday)
-
+# 1: From Airport
+# 2: To Airport
+# 3: Number of Days from Today to start looking from
+# 4: Number of Days to search
+# 5: Email Password
 
 import requests
 import datetime
@@ -18,16 +18,77 @@ import smtplib
 # init smtplib
 to = 'mackey.nichols@gmail.com'
 gmail_user = 'mackey.nichols@gmail.com'
-gmail_pwd = sys.argv[3]
-
-
+gmail_pwd = sys.argv[5]
 
 # init url and api key
 apiKey = "AIzaSyColmjm796njJk4eoDp24ygH64xkeK0q0E"
 url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key="+apiKey
 
-# List of Friday and Sunday Dates
+# init vars for json
+todayDate = datetime.date.today()
+numTickets = 1
+origin = sys.argv[1]
+destination = sys.argv[2]
+daysFromToday = int(sys.argv[3])
+daysToSearch = int(sys.argv[4])
+numResponses = 50
+
+responses = []
+
+# For each day from today + daysFromToday to today + daysFromToday + daysToSearch
+for searchDate in [ datetime.date.today() + datetime.timedelta(days=daysFromToday+i) for i in range(daysToSearch) ]:
+    # create input json
+    inputJson = {
+      "request": {
+        "passengers": {
+          "kind": "qpxexpress#passengerCounts",
+          "adultCount": numTickets,
+          "infantInLapCount": 0,
+          "infantInSeatCount": 0,
+          "childCount": 0,
+          "seniorCount": 0
+        },
+        "slice": [
+          {
+            "kind": "qpxexpress#sliceInput",
+            "origin": origin,
+            "destination": destination,
+            "date": searchDate.strftime("%Y-%m-%d")
+          }
+        ],
+            
+        #"maxPrice": maxPrice,
+        "refundable": "false",
+        "solutions": numResponses
+      }
+    }
+
+    # send json to google flights, parse response json
+    r = json.loads(requests.post(url, json = inputJson).text)
+
+    # for each response in this request, summarize the response and add it to responses array
+    for i in range(len(r['trips']['tripOption'])):
+
+            thisFlight = {
+            "price": r['trips']['tripOption'][i]['saleTotal'],
+            "flightNo": r['trips']['tripOption'][i]['slice'][0]['segment'][0]['flight']['carrier']+r['trips']['tripOption'][i]['slice'][0]['segment'][0]['flight']['number'],
+            "departureFrom"+origin: r['trips']['tripOption'][i]['slice'][0]['segment'][0]['leg'][0]['departureTime']
+            }
+                
+            responses.append(thisFlight)
+            
+           
+pprint.pprint(responses)
+
+'''
+=============
+TRASH
+=============
+
+
 weekends = []
+
+
 if len(sys.argv) < 5:
     departureDay = 4 # Default Friday departure code for below loop
 elif sys.argv[4].lower()[:4] == "thurs":
@@ -45,70 +106,6 @@ for i in range(14,21):
         thisSunday = datetime.date.today() + datetime.timedelta(days = i+2)
         weekends.append({"friday":thisFriday, "sunday":thisSunday})
 
-
-# init vars for json
-numTickets = 1
-origin = "YTZ"
-destination = sys.argv[1]
-maxPrice = "CAD"+str(sys.argv[2])
-numResponses = 50
-#date = year+"-"+month+"-"+day
-
-responses = []
-# For each weekend we search, we send a request
-for weekend in weekends:
-    # create input json
-    inputJson = {
-      "request": {
-        "passengers": {
-          "kind": "qpxexpress#passengerCounts",
-          "adultCount": numTickets,
-          "infantInLapCount": 0,
-          "infantInSeatCount": 0,
-          "childCount": 0,
-          "seniorCount": 0
-        },
-        "slice": [
-          {
-            "kind": "qpxexpress#sliceInput",
-            "origin": origin,
-            "destination": destination,
-            "date": weekend["friday"].strftime("%Y-%m-%d")
-          },
-          {
-            "kind": "qpxexpress#sliceInput",
-            "origin": destination,
-            "destination": origin,
-            "date": weekend["sunday"].strftime("%Y-%m-%d")
-          }
-        ],
-            
-        "maxPrice": maxPrice,
-        "refundable": "false",
-        "solutions": numResponses
-      }
-    }
-
-    # send json to google flights, parse response json
-    r = json.loads(requests.post(url, json = inputJson).text)
-
-    # for each response in this request, find the lowest cost
-    try:
-        for i in range(len(r['trips']['tripOption'])):
-            try:
-                thisFlight = {
-                "price": r['trips']['tripOption'][i]['saleTotal'],
-                "flightNo": r['trips']['tripOption'][i]['slice'][0]['segment'][0]['flight']['carrier']+r['trips']['tripOption'][i]['slice'][0]['segment'][0]['flight']['number'],
-                "departureFromYTZ": r['trips']['tripOption'][i]['slice'][0]['segment'][0]['leg'][0]['departureTime']
-                }
-                    
-                pprint.pprint(thisFlight)
-                responses.append(thisFlight)
-            except:
-                print("Error: "+ sys.exc_info()[0])
-    except:
-            print("no cheap flights found")
-            
 # for each search, modify the json, send request for many dates
 # send text (and email) if certain criteria are met
     # if cheapest flight of all flights found in this run is less than 2nd command line input, send text!
@@ -123,4 +120,5 @@ if float(min(responses, key=lambda x: x['price'])['price'][3:]) < float(sys.argv
     smtpserver.sendmail(gmail_user, to, msg)    
     smtpserver.close()
 
+'''
 
